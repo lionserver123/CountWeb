de0c61076af8f885693059e3e54d613e7c108a30
lionserver123 <59630967+lionserver123@users.noreply.github.com>
2024-07-10 21:41:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let count = 0;
+let count = 0.0;
 let intervalId;
 
 function updateCounter() {
-  count++;
-  document.getElementById("counter").innerText = count;
+  count += 0.01;
+  document.getElementById("counter").innerText = count.toFixed(2);
 }
 
 document.getElementById("startButton").addEventListener("click", function () {
   if (!intervalId) {
-    intervalId = setInterval(updateCounter, 1000);
+    intervalId = setInterval(updateCounter, 10);
   }
   document.getElementById("startButton").style.display = "none";
   document.getElementById("stopButton").style.display = "inline-block";
@@ -24,7 +24,7 @@ document.getElementById("stopButton").addEventListener("click", function () {
 document.getElementById("resetButton").addEventListener("click", function () {
   clearInterval(intervalId);
   intervalId = null;
-  count = 0;
+  count = 0.0;
   document.getElementById("counter").innerText = count;
   document.getElementById("startButton").style.display = "inline-block";
   document.getElementById("stopButton").style.display = "none";
